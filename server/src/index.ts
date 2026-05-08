6688fe67c8e019d9cfcc659e98e44dd05c0f4dfc
piyush5784 <piyushjha5668@gmail.com>
2026-05-08 21:21:58 +0530

    testing
    Signed-off-by: piyush5784 <piyushjha5668@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -115,6 +115,8 @@ app.post("/api/chat/resume", async (req: Request, res: Response) => {
 
 const PORT = process.env.PORT ?? 3001;
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}
 
 export default app;
